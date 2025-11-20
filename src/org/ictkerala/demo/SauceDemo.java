0aae76506b5def826d989ff9d1f29c427d1fa316
ZuhraQureishiN <zuhraqureishi@gmail.com>
2025-11-20 11:40:29 +0530

    First Commit

---
+++ src/org/ictkerala/demo/SauceDemo.java
@@ -0,0 +1,27 @@
+package org.ictkerala.demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class SauceDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.saucedemo.com/");
+		WebElement uname=driver.findElement(By.id("user-name"));
+		uname.sendKeys("standard_user");
+		WebElement passwrd=driver.findElement(By.id("password"));
+		passwrd.sendKeys("secret_sauce");
+		WebElement login=driver.findElement(By.xpath("//input[@id='login-button']"));
+		login.click();
+		}
+
+}
+
+////*[@id="login-button"] - Relative xpath
+///html/body/div/div/div[2]/div[1]/div/div/form/input - Absolute xpath
